3d027ef87889d35788a00624608721e5e4479cc5
Krailon <mcinnes.mj@gmail.com>
2015-01-30 04:35:25 -0800

    Moved globals back to top; Added another debugging LED toggle

--- MoteRunner/MeshMonitor.cs
+++ MoteRunner/MeshMonitor.cs
@@ -4,6 +4,17 @@ namespace TRU.MeshMonitor {
 	
 	public class MeshMonitor {
 		
+		// Globals
+		private const uint ADC_CHANNEL_MASK = 0x02;
+		private const uint REPLY_SIZE = 0x40; // 64
+		private const uint READ_INTERVAL = 5; // 5s 
+		private const byte TEMP_PWR_PIN = IRIS.PIN_PW0; // PC0
+		private const byte LIGHT_PWR_PIN = IRIS.PIN_INT5; // PE5
+		
+		private static byte[] Reply = new byte[REPLY_SIZE];
+		private static ADC ADC_Device = new ADC();
+		private static GPIO GPIO_Device = new GPIO();
+
 		static MeshMonitor() {
 			LED.setState(IRIS.LED_YELLOW, 1);
 			Assembly.setDataHandler(onData);
@@ -28,6 +39,7 @@ namespace TRU.MeshMonitor {
 			uint offset = LIP.getPortOff() + 1;
 			byte[] sensor;
 
+			LED.setState(IRIS.LED_YELLOW, 1);
 			if (GPIO_Device.doPin(GPIO.CTRL_READ, TEMP_PWR_PIN) == 0) {
 				sensor = new byte[] {0, 0}; // 0 => Temperature
 				GPIO_Device.configureOutput(TEMP_PWR_PIN, GPIO.OUT_SET);
@@ -42,20 +54,10 @@ namespace TRU.MeshMonitor {
 			Util.copyData(sensor, 0, Reply, offset, 2);
 			Util.copyData(Data, 0, Reply, offset, 2);
 			LIP.send(Reply, 0, REPLY_SIZE);
+			LED.setState(IRIS.LED_YELLOW, 0);
 
 			return 0;
 		}
-
-		// Globals
-		private const uint ADC_CHANNEL_MASK = 0x02;
-		private const uint REPLY_SIZE = 0x40; // 64
-		private const uint READ_INTERVAL = 5; // 5s 
-		private const byte TEMP_PWR_PIN = IRIS.PIN_PW0; // PC0
-		private const byte LIGHT_PWR_PIN = IRIS.PIN_INT5; // PE5
-		
-		private static byte[] Reply = new byte[REPLY_SIZE];
-		private static ADC ADC_Device = new ADC();
-		private static GPIO GPIO_Device = new GPIO();
 
 	}
 
