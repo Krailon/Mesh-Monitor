23ef9548f43e8f55d96db516d0d9eaa53cfc12d0
Krailon <mcinnes.mj@gmail.com>
2015-01-30 03:42:53 -0800

    Reordering blocks to try and sort out crashing on post-installation initialization

--- MoteRunner/MeshMonitor.cs
+++ MoteRunner/MeshMonitor.cs
@@ -4,36 +4,23 @@ namespace TRU.MeshMonitor {
 	
 	public class MeshMonitor {
 		
-		// Globals
-		private const uint ADC_CHANNEL_MASK = 0x02;
-		private const uint REPLY_SIZE = 0x40; // 64
-		private const uint READ_INTERVAL = 5; // 5s 
-		private const byte TEMP_PWR_PIN = IRIS.PIN_PW0; // PC0
-		private const byte LIGHT_PWR_PIN = IRIS.PIN_INT5; // PE5
-		private const byte YELLOW_LED = 0;
-		private const byte GREEN_LED = 1;
-		private const byte RED_LED = 2;
-		
-		private static byte[] Reply = new byte[REPLY_SIZE];
-		private static ADC ADC_Device = new ADC();
-		private static GPIO GPIO_Device = new GPIO();
-
 		static MeshMonitor() {
-			LED.setState(YELLOW_LED, 1);
-
+			LED.setState(IRIS.LED_YELLOW, 1);
 			Assembly.setDataHandler(onData);
 			GPIO_Device.open();
-			GPIO_Device.configureOutput(TEMP_PWR_PIN, GPIO.OUT_SET);
+			GPIO_Device.configureOutput(LIGHT_PWR_PIN, GPIO.OUT_SET);
 			ADC_Device.open(ADC_CHANNEL_MASK, GPIO.NO_PIN, 0, 0); // Manual power; No warmup; No interval (ltr)
 			ADC_Device.setReadHandler(ADCReadCallback);
 			ADC_Device.read(Device.TIMED, 1, Time.currentTicks() + Time.toTickSpan(Time.SECONDS, READ_INTERVAL));
 
-			LED.setState(YELLOW_LED, 0); 
-			LED.setState(GREEN_LED, 1);
+			LED.setState(IRIS.LED_YELLOW, 0);
+			LED.setState(IRIS.LED_GREEN, 1);
 		}
 
 		private static int onData(uint Info, byte[] Buffer, uint Length) {
+			LED.setState(IRIS.LED_RED, 1);
 			Util.copyData(Buffer, 0, Reply, 0, Length);
+			LED.setState(IRIS.LED_RED, 0);
 			return 0;
 		}
 
@@ -52,12 +39,23 @@ namespace TRU.MeshMonitor {
 				GPIO_Device.configureOutput(TEMP_PWR_PIN, GPIO.OUT_CLR);
 			}
 
-			Util.copyData(sensor, 0, Reply, offset, 2);			
-			Util.copyData(Data, 0, Reply, offset + 2, 2);
+			Util.copyData(sensor, 0, Reply, offset, 2);
+			Util.copyData(Data, 0, Reply, offset, 2);
 			LIP.send(Reply, 0, REPLY_SIZE);
 
 			return 0;
 		}
+
+		// Globals
+		private const uint ADC_CHANNEL_MASK = 0x02;
+		private const uint REPLY_SIZE = 0x40; // 64
+		private const uint READ_INTERVAL = 5; // 5s 
+		private const byte TEMP_PWR_PIN = IRIS.PIN_PW0; // PC0
+		private const byte LIGHT_PWR_PIN = IRIS.PIN_INT5; // PE5
+		
+		private static byte[] Reply = new byte[REPLY_SIZE];
+		private static ADC ADC_Device = new ADC();
+		private static GPIO GPIO_Device = new GPIO();
 
 	}
 
