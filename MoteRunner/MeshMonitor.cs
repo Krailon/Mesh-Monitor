51448554cbb1b4b9a9e4def6d4991533d06a4278
Krailon <mcinnes.mj@gmail.com>
2015-02-12 04:40:00 -0800

    Testing the SimpleDevice class for sensor reads

--- MoteRunner/MeshMonitor.cs
+++ MoteRunner/MeshMonitor.cs
@@ -5,9 +5,10 @@ namespace TRU.MeshMonitor {
 	public class MeshMonitor {
 		
 		// Globals
+        private const uint FLAG_FAILED = 0x40;
 		private const uint ADC_CHANNEL_MASK = 0x02;
 		private const uint REPLY_SIZE = 0x40; // 64
-		private const uint READ_INTERVAL = 5; // 5s 
+		private const uint READ_INTERVAL = 5; // 5s
 		private const byte TEMP_PWR_PIN = IRIS.PIN_PW0; // PC0
 		private const byte LIGHT_PWR_PIN = IRIS.PIN_INT5; // PE5
 		
@@ -15,24 +16,58 @@ namespace TRU.MeshMonitor {
 		private static ADC ADC_Device = new ADC();
 		private static GPIO GPIO_Device = new GPIO();
 
+        private static SDev HumidTempSensor = new SDev();
+        //private static SDev LightSensor = new SDev();
+        //private static SDev AccelSensor = new SDev();
+
 		static MeshMonitor() {
 			LED.setState(IRIS.LED_YELLOW, 1);
 
-			Assembly.setDataHandler(onData);
-			GPIO_Device.open();
-			GPIO_Device.configureOutput(LIGHT_PWR_PIN, GPIO.OUT_SET);
-			ADC_Device.open(ADC_CHANNEL_MASK, GPIO.NO_PIN, 0, 0); // Manual power; No warmup; No interval (ltr)
-			ADC_Device.setReadHandler(ADCReadCallback);
-			ADC_Device.read(Device.TIMED, 1, Time.currentTicks() + Time.toTickSpan(Time.SECONDS, READ_INTERVAL));
+			Assembly.setDataHandler(on_Data);
+            try {
+                HumidTempSensor.open(IRIS.DID_MTS400_HUMID_TEMP, null, 0, 0);
+                HumidTempSensor.setReadHandler(ADC_Read_Callback);
+                HumidTempSensor.read(Device.TIMED, 4, Time.currentTicks() + Time.toTickSpan(Time.SECONDS, READ_INTERVAL));
 
-			LED.setState(IRIS.LED_YELLOW, 0);
-			LED.setState(IRIS.LED_GREEN, 1);
+                LED.setState(IRIS.LED_YELLOW, 0);
+			    LED.setState(IRIS.LED_GREEN, 1);
+            }
+            catch {
+                LED.setState(IRIS.LED_RED, 1);
+                return;
+            }
+
+			//GPIO_Device.open();
+			//GPIO_Device.configureOutput(LIGHT_PWR_PIN, GPIO.OUT_SET);
+			//ADC_Device.open(ADC_CHANNEL_MASK, GPIO.NO_PIN, 0, 0); // Manual power; No warmup; No interval (ltr)
+			//ADC_Device.setReadHandler(ADCReadCallback);
+			//ADC_Device.read(Device.TIMED, 1, Time.currentTicks() + Time.toTickSpan(Time.SECONDS, READ_INTERVAL));
 		}
 
-		private static int onData(uint Info, byte[] Buffer, uint Length) {
+		private static int on_Data(uint Info, byte[] Buffer, uint Length) {
 			Util.copyData(Buffer, 0, Reply, 0, Length);
 			return 0;
 		}
+
+        private static int ADC_Read_Callback(uint ReadFlags, byte[] ReadData, uint ReadLength, uint ReadInfo, long ReadTime) {
+            // Check flags
+            if ((ReadFlags & FLAG_FAILED) != 0) {
+                LED.setState(IRIS.LED_YELLOW, 1);
+                LED.setState(IRIS.LED_RED, 1);
+            }
+            else {
+                uint payload_offset = LIP.getPortOff() + 1;
+
+                LED.setState(IRIS.LED_YELLOW, 1);
+                Util.copyData(new byte[] { 0 }, 0, Reply, payload_offset, 1); // Humidit+Temperature ID = 0
+                Util.copyData(ReadData, 0, Reply, payload_offset + 1, ReadLength);
+                LIP.send(Reply, 0, REPLY_SIZE);
+                HumidTempSensor.read(Device.TIMED, 4, Time.currentTicks() + Time.toTickSpan(Time.SECONDS, READ_INTERVAL));
+                LED.setState(IRIS.LED_YELLOW, 0);
+            }
+
+            return 0;
+        }
 
 		private static int ADCReadCallback(uint ReadFlags, byte[] ReadData, uint ReadLength, uint ReadInfo, long ReadTime) {
 			uint offset = LIP.getPortOff() + 1;
