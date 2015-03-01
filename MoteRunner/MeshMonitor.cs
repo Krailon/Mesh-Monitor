a68978c996d5d0cad78aab8cbe69e19abc2a9bc4
Krailon <mcinnes.mj@gmail.com>
2015-03-01 05:23:30 -0800

    Removed lingering legacy code

--- MoteRunner/MeshMonitor.cs
+++ MoteRunner/MeshMonitor.cs
@@ -7,16 +7,10 @@ namespace TRU.MeshMonitor {
 		
 		// Globals
         private const uint FLAG_FAILED = 0x40;
-		private const uint ADC_CHANNEL_MASK = 0x02;
 		private const uint REPLY_SIZE = 0x40; // 64
 		private const uint READ_INTERVAL = 5; // 5s
-		private const byte TEMP_PWR_PIN = IRIS.PIN_PW0; // PC0
-		private const byte LIGHT_PWR_PIN = IRIS.PIN_INT5; // PE5
 		
 		private static byte[] Reply = new byte[REPLY_SIZE];
-		private static ADC ADC_Device = new ADC();
-		private static GPIO GPIO_Device = new GPIO();
-
         private static SDev HumidTempSensor = new SDev();
         private static SDev LightSensor = new SDev();
         private static SDev AccelSensor = new SDev();
@@ -134,10 +128,6 @@ namespace TRU.MeshMonitor {
                     // Queue accelerometer sensor read
                     AccelSensor.open(IRIS.DID_MTS400_ACCEL, null, 0, 0);
                     AccelSensor.read(Device.ASAP, 4, 0);
-
-                    // Queue humidity/temperature sensor read
-                    //HumidTempSensor.open(IRIS.DID_MTS400_HUMID_TEMP, null, 0, 0);
-                    //HumidTempSensor.read(Device.TIMED, 4, Time.currentTicks() + Time.toTickSpan(Time.SECONDS, READ_INTERVAL));
                 }
                 catch (MoteException ex) {
                     LED.setState(IRIS.LED_RED, 1);
@@ -188,33 +178,6 @@ namespace TRU.MeshMonitor {
                 return 0;
             }
         }
-
-		/*
-        private static int ADCReadCallback(uint ReadFlags, byte[] ReadData, uint ReadLength, uint ReadInfo, long ReadTime) {
-			uint offset = LIP.getPortOff() + 1;
-			byte[] sensor;
-
-			LED.setState(IRIS.LED_YELLOW, 1);
-			if (GPIO_Device.doPin(GPIO.CTRL_READ, TEMP_PWR_PIN) == 0) {
-				sensor = new byte[] {0, 0}; // 0 => Temperature
-				GPIO_Device.configureOutput(TEMP_PWR_PIN, GPIO.OUT_SET);
-				GPIO_Device.configureOutput(LIGHT_PWR_PIN, GPIO.OUT_CLR);
-			}
-			else {
-				sensor = new byte[] {1, 0}; // 1 => Light
-				GPIO_Device.configureOutput(LIGHT_PWR_PIN, GPIO.OUT_SET);
-				GPIO_Device.configureOutput(TEMP_PWR_PIN, GPIO.OUT_CLR);
-			}
-
-			Util.copyData(sensor, 0, Reply, offset, 2);
-			Util.copyData(ReadData, 0, Reply, offset + 2, 2);
-			LIP.send(Reply, 0, REPLY_SIZE);
-
-            ADC_Device.read(Device.TIMED, 1, Time.currentTicks() + Time.toTickSpan(Time.SECONDS, READ_INTERVAL));
-			LED.setState(IRIS.LED_YELLOW, 0);
-            return 0;
-		}
-         */
 
 	}
 
